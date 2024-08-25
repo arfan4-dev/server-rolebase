bae02560e98611d3c6411f05812d1ca3c2ac082c
Arshan Nawaz <arshannawaz01@gmail.com>
2024-08-26 01:06:03 +0500

    chore: Remove some code to test pre-commit

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -21,3 +21,22 @@ module.exports = {
         'subject-case': [2, 'always', 'sentence-case']
     }
 }
+// for flexible commit messages
+// module.exports = {
+//     extends: ['@commitlint/cli', '@commitlint/config-conventional'],
+//     rules: {
+//         // Disable the type-enum rule
+//         'type-enum': [0], // 0 disables the rule
+
+//         // Disable the type-empty rule to allow commits without a type
+//         'type-empty': [0], // 0 disables the rule
+
+//         // Disable the subject-case rule
+//         'subject-case': [0], // 0 disables the rule
+
+//         // Optionally, disable subject-empty to allow empty subjects
+//         'subject-empty': [0], // 0 disables the rule
+
+//         // Disable other rules if needed
+//     }
+// }
